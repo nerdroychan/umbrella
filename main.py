c60e0c2612fa18e85d1d614df40ca00fd19a82ba
Roy Chan <i@roychan.org>
2015-06-06 01:00:50 +0800

    clean

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import tornado.options
 import tornado.httpclient
 import tornado.gen
 import json
+import os.path
 
 from tornado.options import define, options
 define('port', default = 8967, help = "run on the given port", type = int)
@@ -27,9 +28,9 @@ class IndexHandler(tornado.web.RequestHandler):
         weather = int(weatherInfo['list'][0]['weather'][0]['id'])
         umbrella = [200, 201, 202, 210, 211, 212, 221, 230, 231, 232, 300, 301, 302, 310, 311, 312, 313, 314, 321, 500, 501, 502, 503, 504, 511, 520, 521, 522, 531, 600, 601, 602, 611, 612, 615, 616, 620, 621, 622, 781, 900, 901, 902, 906]
         if weather in umbrella:
-            self.write('记得带伞')
+            self.render('index.html', value = 'Yes!')
         else:
-            self.write('不用带伞')
+            self.render('index.html', value = str(weather) + 'No.')
         self.finish()
 
 if __name__ == '__main__':
@@ -37,7 +38,8 @@ if __name__ == '__main__':
     app = tornado.web.Application(
         handlers = [
             (r'^/$', IndexHandler),    
-        ]
+        ],
+        template_path=os.path.join(os.path.dirname(__file__), "templates"),
     )
     httpServer = tornado.httpserver.HTTPServer(app)
     httpServer.listen(options.port)
