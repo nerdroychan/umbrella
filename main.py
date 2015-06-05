f76d895a6d9bae303f13ea4fc012f83600d308d8
Roy Chan <i@roychan.org>
2015-06-05 21:02:17 +0800

    hw

---
+++ main.py
@@ -0,0 +1,34 @@
+import tornado.ioloop
+import tornado.web
+import tornado.httpserver
+import tornado.options
+import tornado.httpclient
+import tornado.gen
+
+from tornado.options import define, options
+define('port', default = 8967, help = "run on the given port", type = int)
+
+'''class IndexHandler(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
+    @tornado.gen.engine
+    def get(self):
+        client = tornado.httpclient.AsyncHTTPClient()
+        response = yield tornado.gen.Task(client.fetch, 'http://www.weibo.cn')
+        self.write('hello, world')
+        self.finish()'''
+
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self):
+        ipAddr = self.request.remote_ip
+        self.write(ipAddr)
+
+if __name__ == '__main__':
+    tornado.options.parse_command_line()
+    app = tornado.web.Application(
+        handlers = [
+            (r'^/$', IndexHandler),    
+        ]
+    )
+    httpServer = tornado.httpserver.HTTPServer(app)
+    httpServer.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
