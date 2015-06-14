76d6b3899cbfcac744d292477033938a09e71204
Roy Chan <i@roychan.org>
2015-06-14 17:32:45 +0800

    full expection

--- main.py
+++ main.py
@@ -15,23 +15,26 @@ class IndexHandler(tornado.web.RequestHandler):
     @tornado.gen.engine
     def get(self):
         try:
-            ipAddr = self.request.headers['X-Forwarded-For']
-        except KeyError:
-            ipAddr = self.request.remote_ip
+            try:
+                ipAddr = self.request.headers['X-Forwarded-For']
+            except KeyError:
+                ipAddr = self.request.remote_ip
 
-        client = tornado.httpclient.AsyncHTTPClient()
-        geoIpResponse = yield tornado.gen.Task(client.fetch, 'https://freegeoip.net/json/' + ipAddr)
-        geoInfo = json.loads(geoIpResponse.body.decode('utf-8'))
-        latitude, longitude = str(geoInfo['latitude']), str(geoInfo['longitude'])
-        weatherResponse = yield tornado.gen.Task(client.fetch, 'http://api.openweathermap.org/data/2.5/forecast/daily?mode=json&cnt=1&' + 'lon=' + longitude + '&lat=' + latitude)
-        weatherInfo = json.loads(weatherResponse.body.decode('utf-8'))
-        weather = int(weatherInfo['list'][0]['weather'][0]['id'])
-        umbrella = [200, 201, 202, 210, 211, 212, 221, 230, 231, 232, 300, 301, 302, 310, 311, 312, 313, 314, 321, 500, 501, 502, 503, 504, 511, 520, 521, 522, 531, 600, 601, 602, 611, 612, 615, 616, 620, 621, 622, 781, 900, 901, 902, 906]
-        if weather in umbrella:
-            self.render('index.html', value = 'Yes!')
-        else:
-            self.render('index.html', value = 'No.')
-        self.finish()
+            client = tornado.httpclient.AsyncHTTPClient()
+            geoIpResponse = yield tornado.gen.Task(client.fetch, 'https://freegeoip.net/json/' + ipAddr)
+            geoInfo = json.loads(geoIpResponse.body.decode('utf-8'))
+            latitude, longitude = str(geoInfo['latitude']), str(geoInfo['longitude'])
+            weatherResponse = yield tornado.gen.Task(client.fetch, 'http://api.openweathermap.org/data/2.5/forecast/daily?mode=json&cnt=1&' + 'lon=' + longitude + '&lat=' + latitude)
+            weatherInfo = json.loads(weatherResponse.body.decode('utf-8'))
+            weather = int(weatherInfo['list'][0]['weather'][0]['id'])
+            umbrella = [200, 201, 202, 210, 211, 212, 221, 230, 231, 232, 300, 301, 302, 310, 311, 312, 313, 314, 321, 500, 501, 502, 503, 504, 511, 520, 521, 522, 531, 600, 601, 602, 611, 612, 615, 616, 620, 621, 622, 781, 900, 901, 902, 906]
+            if weather in umbrella:
+                self.render('index.html', value = 'Yes!')
+            else:
+                self.render('index.html', value = 'No.')
+            self.finish()
+        except:
+            self.render('index.html', value = 'Unexpected Error, Please refresh.')
 
 if __name__ == '__main__':
     tornado.options.parse_command_line()
